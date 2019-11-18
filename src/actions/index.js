a09dcec58f08240fa0bb7348353e85cad003142d
callmeccy <callmeccy@gmail.com>
2019-11-18 23:33:42 +0900

    implement action and action creator

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export const INCREMENT = 'INCREMENT'
+export const DECRREMENT = 'DECREMENT'
+
+export const increment = () => ({
+    type: INCREMENT
+})
+
+export const decrement = () => ({
+    type: DECRREMENT
+})
