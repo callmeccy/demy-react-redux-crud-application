e0498e12f4fccdc1d94791ffbd60f2c7eb43e4dc
callmeccy <callmeccy@gmail.com>
2019-11-19 20:38:45 +0900

    done

---
+++ src/reducers/count.js
@@ -0,0 +1,14 @@
+import { INCREMENT, DECRREMENT } from '../actions'
+
+const initialState = { value: 0 }
+
+export default (state = initialState, action) => {
+    switch (action.type){
+        case INCREMENT:
+            return {value: state.value + 1}
+        case DECRREMENT:
+            return {value: state.value - 1}
+        default:
+            return state;
+    }
+}
